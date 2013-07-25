ef57475f5df662258798e3b8c54bfd6c156fbf90
Mitchell Stokes <mogurijin@gmail.com>
2013-07-25 00:09:23 -0700

    Adding some basic movement controls for the character.

--- src/levels/main.py
+++ src/levels/main.py
@@ -2,7 +2,8 @@ import sys
 import os
 
 
-from bge import logic
+from bge import logic, events
+from mathutils import Vector
 
 
 def init():
@@ -16,6 +17,23 @@ def init():
 
 def run():
 	try:
-		logic.character.update()
+		logic.character
 	except AttributeError:
 		init()
+
+	logic.character.update()
+
+	movevec = Vector((0, 0))
+
+	for key, status in logic.keyboard.active_events.items():
+		if key == events.WKEY:
+			movevec += Vector((0, 1))
+		elif key == events.AKEY:
+			movevec += Vector((-1, 0))
+		elif key == events.SKEY:
+			movevec += Vector((0, -1))
+		elif key == events.DKEY:
+			movevec += Vector((1, 0))
+
+	logic.character.move(movevec)
+
