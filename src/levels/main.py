8462c7991104cba55a76ff15a7d32de6eb861cf2
Mitchell Stokes <mogurijin@gmail.com>
2013-07-25 01:52:42 -0700

    Movement is now based on the camera's orientation, however, I don't realy have a good way to test it yet, so it may not work correctly.

--- src/levels/main.py
+++ src/levels/main.py
@@ -23,19 +23,22 @@ def run():
 
 	logic.character.update()
 
-	movevec = Vector((0, 0))
+	cam = logic.getCurrentScene().active_camera
+	cam_mat = cam.worldOrientation
+
+	movevec = Vector((0, 0, 0))
 
 	for key, status in logic.keyboard.active_events.items():
 		if key == events.WKEY:
-			movevec += Vector((0, 1))
+			movevec += Vector((0, 1, 0))
 		elif key == events.AKEY:
-			movevec += Vector((-1, 0))
+			movevec += Vector((-1, 0, 0))
 		elif key == events.SKEY:
-			movevec += Vector((0, -1))
+			movevec += Vector((0, -1, 0))
 		elif key == events.DKEY:
-			movevec += Vector((1, 0))
+			movevec += Vector((1, 0, 0))
 		elif key == events.SPACEKEY and status == logic.KX_INPUT_JUST_ACTIVATED:
 			logic.character.jump()
 
-	logic.character.move(movevec)
+	logic.character.move(movevec.normalized().xy)
 
