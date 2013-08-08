5dba335a767d2be104ac9ac01fd0e4a5833cb262
Mitchell Stokes <mogurijin@gmail.com>
2013-08-08 02:09:25 -0700

    Moving a majority of the main.py code over to the DefaultState.

--- src/levels/main.py
+++ src/levels/main.py
@@ -1,17 +1,7 @@
 import sys
 import os
-import math
 
-
-from bge import logic, events
-from mathutils import Vector, Euler
-
-
-
-CAM_MAX_DIST = 9
-CAM_MIN_DIST = 7
-CAM_DOWN_LIMIT = 0.5
-CAM_UP_LIMIT = -1.1
+from bge import logic
 
 
 def init():
@@ -20,135 +10,23 @@ def init():
 		sys.path.append("..")
 
 		try:
-			from scripts.character import UllurCharacter, Meatsack
-			logic.character = UllurCharacter.spawn()
-
-			logic.meatsacks = []
-			for i in [i for i in logic.getCurrentScene().objects if i.name.startswith('MeatsackSpawn')]:
-				logic.meatsacks.append(Meatsack.spawn(i.worldPosition, i.worldOrientation))
-		except:
-			import traceback
-			traceback.print_exc()
-			logic.character = None
-
-		try:
-			from scripts.ai.manager import Manager
-			from scripts.ai.agent_bge import AgentBGE
-			logic.ai_system = Manager()
-			if logic.character:
-				target = AgentBGE(logic.character)
-				for meatsack in logic.meatsacks:
-					agent = AgentBGE(meatsack)
-					agent.target = target
-					agent.load_definition("../scripts/ai/definitions/state_test.json")
-					logic.ai_system._agents.append(agent)
-
-		except:
-			import traceback
-			traceback.print_exc()
-			logic.ai_system = None
-
-		try:
-			from scripts.collectable import CollectableSensor
-			logic.collectables = []
-			for i in [i for i in logic.getCurrentScene().objects if i.name.startswith('Collectable')]:
-				if i.groupObject:
-					logic.collectables.append(CollectableSensor(i))
-		except:
-			import traceback
-			traceback.print_exc()
-
-		try:
 			from scripts.state import StateSystem, DefaultState
 			logic.state_system = StateSystem(DefaultState)
 		except:
 			import traceback
 			traceback.print_exc()
-
-		logic.mouse.position = (0.5, 0.5)
+			logic.state_system = None
 
 
 def run():
 	try:
-		logic.character
+		logic.state_system
 	except AttributeError:
 		init()
 
-	if not logic.character:
+	if not logic.state_system:
 		return
 
 	logic.state_system.update()
 
-	logic.character.update()
-
-	cam = logic.getCurrentScene().active_camera
-
-	movevec = Vector((0, 0, 0))
-
-	for key, status in logic.keyboard.active_events.items():
-		if key == events.WKEY:
-			movevec += Vector((0, 1, 0))
-		elif key == events.AKEY:
-			movevec += Vector((-1, 0, 0))
-		elif key == events.SKEY:
-			movevec += Vector((0, -1, 0))
-		elif key == events.DKEY:
-			movevec += Vector((1, 0, 0))
-		elif key == events.SPACEKEY and status == logic.KX_INPUT_JUST_ACTIVATED:
-			logic.character.jump()
-
-	for event, status in logic.mouse.active_events.items():
-		if event == events.LEFTMOUSE and status == logic.KX_INPUT_JUST_ACTIVATED:
-			logic.character.attack("LEFT")
-		elif event == events.RIGHTMOUSE and status == logic.KX_INPUT_JUST_ACTIVATED:
-			logic.character.attack("RIGHT")
-
-	dx = 0.5 - logic.mouse.position[0]
-	dy = 0.5 - logic.mouse.position[1]
-
-	if cam.parent:
-		# X Movement
-		if math.fabs(dx) > 0.01:
-			cam.parent.applyRotation((0, 0, dx))
-
-		# Y Movement
-		cam_angle = cam.parent.localOrientation.to_euler('XYZ')
-		move_down_limit = dy > 0 and cam_angle.x < CAM_DOWN_LIMIT
-		move_up_limit = dy < 0 and cam_angle.x > CAM_UP_LIMIT
-		if math.fabs(dy) > 0.01 and (move_down_limit or move_up_limit):
-			cam.parent.applyRotation((dy/2, 0, 0), True)
-
-		# Zoom in camera based on y movement
-		if cam_angle.x > 0:
-			fac = cam_angle.x / CAM_DOWN_LIMIT
-			cam.localPosition.y = -((CAM_MAX_DIST - CAM_MIN_DIST) * (1 - fac) + CAM_MIN_DIST)
-		else:
-			cam.localPosition.y = -CAM_MAX_DIST
-
-	logic.mouse.position = (0.5, 0.5)
-
-	cam_vec = cam.getAxisVect((0, 0, -1))
-	cam_vec.z = 0
-	offset = cam_vec.angle(Vector((0, 1, 0)))
-	if cam_vec.x > 0:
-		offset *= -1
-	offset = Euler((0, 0, offset))
-	movevec.rotate(offset)
-
-	logic.character.move(movevec.xy)
-
-	dt = 1 / logic.getAverageFrameRate()
-	if logic.ai_system:
-		logic.ai_system.update(dt)
-
-	# Update meatsacks
-	for i in logic.meatsacks[:]:
-		i.update()
-		if i.is_dead:
-			logic.meatsacks.remove(i)
-			i.endObject()
-
-	if len(logic.collectables) == len(logic.character.collectables):
-		logic.collectables = []
-		print("All collectibles gathered.")
 
--- src/scripts/state.py
+++ src/scripts/state.py
@@ -10,7 +10,116 @@ class StateSystem:
 			self.state = next_state()
 
 
+# Try to keep Ullur specific stuff below this line to make it easier to separate later
+import math
+from bge import logic, events
+from mathutils import Vector, Euler
+from .character import UllurCharacter, Meatsack
+from .ai.manager import Manager
+from .ai.agent_bge import AgentBGE
+from .collectable import CollectableSensor
+
+
 class DefaultState:
+	CAM_MAX_DIST = 9
+	CAM_MIN_DIST = 7
+	CAM_DOWN_LIMIT = 0.5
+	CAM_UP_LIMIT = -1.1
+
+	def __init__(self):
+		self.character = UllurCharacter.spawn()
+
+		self.meatsacks = []
+		for i in [i for i in logic.getCurrentScene().objects if i.name.startswith('MeatsackSpawn')]:
+			self.meatsacks.append(Meatsack.spawn(i.worldPosition, i.worldOrientation))
+
+		self.ai_system = Manager()
+		target = AgentBGE(self.character)
+		for meatsack in self.meatsacks:
+			agent = AgentBGE(meatsack)
+			agent.target = target
+			agent.load_definition("../scripts/ai/definitions/state_test.json")
+			self.ai_system._agents.append(agent)
+
+		self.collectables = []
+		for i in [i for i in logic.getCurrentScene().objects if i.name.startswith('Collectable')]:
+			if i.groupObject:
+				self.collectables.append(CollectableSensor(i))
+
+		logic.mouse.position = (0.5, 0.5)
+
 	def update(self):
-		print("Update")
+		self.character.update()
+
+		cam = logic.getCurrentScene().active_camera
+
+		movevec = Vector((0, 0, 0))
+
+		for key, status in logic.keyboard.active_events.items():
+			if key == events.WKEY:
+				movevec += Vector((0, 1, 0))
+			elif key == events.AKEY:
+				movevec += Vector((-1, 0, 0))
+			elif key == events.SKEY:
+				movevec += Vector((0, -1, 0))
+			elif key == events.DKEY:
+				movevec += Vector((1, 0, 0))
+			elif key == events.SPACEKEY and status == logic.KX_INPUT_JUST_ACTIVATED:
+				self.character.jump()
+
+		for event, status in logic.mouse.active_events.items():
+			if event == events.LEFTMOUSE and status == logic.KX_INPUT_JUST_ACTIVATED:
+				self.character.attack("LEFT")
+			elif event == events.RIGHTMOUSE and status == logic.KX_INPUT_JUST_ACTIVATED:
+				self.character.attack("RIGHT")
+
+		dx = 0.5 - logic.mouse.position[0]
+		dy = 0.5 - logic.mouse.position[1]
+
+		if cam.parent:
+			# X Movement
+			if math.fabs(dx) > 0.01:
+				cam.parent.applyRotation((0, 0, dx))
+
+			# Y Movement
+			cam_angle = cam.parent.localOrientation.to_euler('XYZ')
+			move_down_limit = dy > 0 and cam_angle.x < self.CAM_DOWN_LIMIT
+			move_up_limit = dy < 0 and cam_angle.x > self.CAM_UP_LIMIT
+			if math.fabs(dy) > 0.01 and (move_down_limit or move_up_limit):
+				cam.parent.applyRotation((dy/2, 0, 0), True)
+
+			# Zoom in camera based on y movement
+			if cam_angle.x > 0:
+				fac = cam_angle.x / self.CAM_DOWN_LIMIT
+				cam.localPosition.y = -((self.CAM_MAX_DIST - self.CAM_MIN_DIST) * (1 - fac) + self.CAM_MIN_DIST)
+			else:
+				cam.localPosition.y = -self.CAM_MAX_DIST
+
+		logic.mouse.position = (0.5, 0.5)
+
+		cam_vec = cam.getAxisVect((0, 0, -1))
+		cam_vec.z = 0
+		offset = cam_vec.angle(Vector((0, 1, 0)))
+		if cam_vec.x > 0:
+			offset *= -1
+		offset = Euler((0, 0, offset))
+		movevec.rotate(offset)
+
+		self.character.move(movevec.xy)
+
+		dt = 1 / logic.getAverageFrameRate()
+		if self.ai_system:
+			self.ai_system.update(dt)
+
+		# Update meatsacks
+		for i in self.meatsacks[:]:
+			i.update()
+			if i.is_dead:
+				self.meatsacks.remove(i)
+				i.endObject()
+
+		if len(self.collectables) == len(self.character.collectables):
+			self.collectables = []
+			print("All collectibles gathered.")
+
 
