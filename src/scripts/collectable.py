f446e23193d0d9062806821601c7a17d382cd90c
Mitchell Stokes <mogurijin@gmail.com>
2013-08-09 19:45:24 -0700

    Adding the ability to have unique collectables, and adding a speed boosting collectable as an example.

--- src/scripts/collectable.py
+++ src/scripts/collectable.py
@@ -5,20 +5,32 @@ from .character import UllurCharacter
 
 
 class Collectable:
-	pass
+	def __init__(self, character):
+		print(self.__class__.__name__, "added to", character)
+
+
+class CollectableSpeed(Collectable):
+	def __init__(self, character):
+		super().__init__(character)
+
+		character.RUN_MULTIPLIER = 5
 
 class CollectableSensor(types.KX_GameObject):
 	def __init__(self, gameobj, collectable_list):
 		self.collisionCallbacks.append(self.collision)
-		self.collectable = Collectable
 		self.collectable_list = collectable_list
+
+		if self.name.startswith('CollectableSpeed'):
+			self.collectable = CollectableSpeed
+		else:
+			self.collectable = Collectable
 
 	def __new__(cls, gameobj, *args):
 		return super().__new__(cls, gameobj)
 
 	def collision(self, other):
 		if isinstance(other, UllurCharacter):
-			other.add_collectable(self.collectable())
+			other.add_collectable(self.collectable(other))
 			self.collectable_list.remove(self)
 
 			self.endObject()
