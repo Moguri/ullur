abf07cf11e8ff1a2a64a905e2f1d938639289ad8
Mitchell Stokes <mogurijin@gmail.com>
2013-08-12 05:34:16 -0700

    Docs: Adding docstrings to the collectable module.

--- src/scripts/collectable.py
+++ src/scripts/collectable.py
@@ -5,19 +5,27 @@ from .character import UllurCharacter
 
 
 class Collectable:
+	"""Base collectable class"""
 	def __init__(self, character):
 		print(self.__class__.__name__, "added to", character)
 
 
 class CollectableSpeed(Collectable):
+	"""Collectable that increases the character's :attr:`.Character.RUN_MULTIPLIER`"""
 	def __init__(self, character):
 		super().__init__(character)
 
 		character.RUN_MULTIPLIER = 5
 
 class CollectableSensor(types.KX_GameObject):
+	"""Sensor that detects collisions for :class:`.Collectable` objects"""
+
 	def __init__(self, gameobj, collectable_list):
-		self.collisionCallbacks.append(self.collision)
+		"""
+		:param gameobj: The KX_GameObject to mutate (passed on to __new__)
+		:param collectable_list: The list this sensor should be stored in
+		"""
+		self.collisionCallbacks.append(self._collision)
 		self.collectable_list = collectable_list
 
 		if self.name.startswith('CollectableSpeed'):
@@ -28,7 +36,7 @@ class CollectableSensor(types.KX_GameObject):
 	def __new__(cls, gameobj, *args):
 		return super().__new__(cls, gameobj)
 
-	def collision(self, other):
+	def _collision(self, other):
 		if isinstance(other, UllurCharacter):
 			other.add_collectable(self.collectable(other))
 			self.collectable_list.remove(self)
@@ -37,6 +45,11 @@ class CollectableSensor(types.KX_GameObject):
 
 
 def mutate_collectables(objects, collectable_list):
+	"""Mutates a list of KX_GameObjects into :class:`.CollectableSensor`
+
+	:param objects: The list of objects to mutate
+	:param collectable_list: The list to store the mutated collectables
+	"""
 	for i in [i for i in objects if i.name.startswith('Collectable')]:
 		if i.groupObject:
 			collectable_list.append(CollectableSensor(i, collectable_list))
